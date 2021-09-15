9bd80e8acaadba7d4a781610eb759d5506049f78
Arunima Gupta <arunimagupta71@gmail.com>
2021-09-15 11:03:15 +0530

    Basic Operations of a BST using Inorder Traversal

---
+++ InOrderTraversal.py
@@ -0,0 +1,44 @@
+class Tree:
+    def __init__(self,initval = None):
+        self.value = initval
+        if self.value:
+            self.left = Tree()
+            self.right = Tree()
+        else:
+            self.left = None
+            self.right = None
+            
+    def insert(self,v):
+        if self.isempty():
+            self.value = v
+            self.left = Tree()
+            self.right = Tree()
+        if self.value==v:
+            return()
+        elif v<self.value:
+            self.left.insert(v)
+            return
+        else:
+            self.right.insert(v)
+            return
+            
+    
+    def __str__(self):
+        return(str(self.inorder()))
+    
+    def isempty(self):
+            return(self.value==None)
+        
+    
+    def inorder(self):
+        if self.isempty():
+            return([])
+        else:
+            return(self.left.inorder()+[self.value]+self.right.inorder())
+        
+
+t = Tree()      
+for i in [1,65,8,6,7,3,4,55,66,78]:
+    t.insert(i)
+
+print(t)
